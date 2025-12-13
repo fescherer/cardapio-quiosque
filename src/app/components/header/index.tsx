f10df5b70b71e99653df2e22287a3d22363142f0
unknown <felipescherer25@gmail.com>
2025-12-12 21:48:59 -0300

    :wrench: chore test lint

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,15 +1,15 @@
 export function Header() {
-    return (
-        <header className="grid grid-cols-3 p-4">
-            <h1>Quiosque do Parque</h1>
+  return (
+    <header className="grid grid-cols-3 p-4">
+      <h1>Quiosque do Parque</h1>
 
-            <div className="flex gap-4">
-                <button className="border p-2 rounded">Bebidas</button>
-                <button>Salgados</button>
-                <button>Doces</button>
-            </div>
+      <div className="flex gap-4">
+        <button className="rounded-sm border p-2">Bebidas</button>
+        <button>Salgados</button>
+        <button>Doces</button>
+      </div>
 
-            <div className="flex-1"></div>
-        </header>
-    )
+      <div className="flex-1"></div>
+    </header>
+  );
 }
